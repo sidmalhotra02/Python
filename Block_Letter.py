56411e1e17e47bc88abbc78acec7d198dc7a7b4f
Siddhartha Malhotra <117125797+sidmalhotra02@users.noreply.github.com>
2023-07-12 15:27:19 +0530

    Create Block_Letter.py

---
+++ Block_Letter.py
@@ -0,0 +1,10 @@
+# siddhartha malhotra
+# im 21
+
+print(' SSSS    M          M')
+print(' S       M M      M M')
+print(' S       M  M    M  M')
+print(' SSSS    M   M M    M')
+print('    S    M          M')
+print('    S    M          M')
+print(' SSSS    M          M')
